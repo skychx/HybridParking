de1dc4da8d5c20327c553d0543840852f4ada569
skychx <skychx@hotmail.com>
2022-02-27 16:50:58 +0800

    feat(perf-monitor): add terser

--- packages/perf-monitor/rollup.config.js
+++ packages/perf-monitor/rollup.config.js
@@ -3,6 +3,9 @@ import serve from "rollup-plugin-serve";
 import typescript from "@rollup/plugin-typescript";
 import livereload from "rollup-plugin-livereload";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+
+const isDev = process.env.ROLLUP_WATCH;
 
 export default {
   input: "src/index.ts",
@@ -11,20 +14,22 @@ export default {
     format: "iife",
   },
   plugins: [
-    html({
-      meta: [
-        {
-          name: "viewport",
-          content:
-            "width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0",
-        },
-      ],
-    }),
+    nodeResolve(),
+    isDev &&
+      html({
+        meta: [
+          {
+            name: "viewport",
+            content:
+              "width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0",
+          },
+        ],
+      }),
     typescript({
       compilerOptions: { lib: ["es5", "es6", "dom"], target: "es5" },
     }),
-    serve("dist"),
-    livereload(),
-    nodeResolve(),
+    isDev && serve("dist"),
+    isDev && livereload(),
+    !isDev && terser(),
   ],
 };
