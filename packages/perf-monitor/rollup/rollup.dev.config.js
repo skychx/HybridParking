591a15e5eb1b75cf496cdb24000b29a575f6224a
skychx <skychx@hotmail.com>
2022-03-02 22:37:39 +0800

    ci: build js

--- packages/perf-monitor/rollup/rollup.dev.config.js
+++ packages/perf-monitor/rollup/rollup.dev.config.js
@@ -3,9 +3,6 @@ import serve from "rollup-plugin-serve";
 import typescript from "@rollup/plugin-typescript";
 import livereload from "rollup-plugin-livereload";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
-
-const isDev = process.env.ROLLUP_WATCH;
 
 export default {
   input: "src/index.ts",
@@ -15,21 +12,19 @@ export default {
   },
   plugins: [
     nodeResolve(),
-    isDev &&
-      html({
-        meta: [
-          {
-            name: "viewport",
-            content:
-              "width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0",
-          },
-        ],
-      }),
+    html({
+      meta: [
+        {
+          name: "viewport",
+          content:
+            "width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0",
+        },
+      ],
+    }),
     typescript({
       compilerOptions: { lib: ["es5", "es6", "dom"], target: "es5" },
     }),
-    isDev && serve("dist"),
-    isDev && livereload(),
-    !isDev && terser(),
+    serve("dist"),
+    livereload(),
   ],
 };
---
+++ packages/perf-monitor/rollup/rollup.prod.config.js
@@ -0,0 +1,42 @@
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+
+const plugins = [
+  nodeResolve(),
+  typescript({
+    compilerOptions: { lib: ["es5", "es6", "dom"], target: "es5" },
+  }),
+  terser(),
+];
+
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      format: "esm",
+      file: "dist/perf-monitor.js",
+    },
+    plugins: plugins,
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      format: "umd",
+      file: "dist/perf-monitor.umd.js",
+      name: 'perf_monitor',
+      extend: true,
+    },
+    plugins: plugins,
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      format: "iife",
+      file: "dist/perf-monitor.iife.js",
+      name: 'perf_monitor',
+      extend: true,
+    },
+    plugins: plugins,
+  },
+];
